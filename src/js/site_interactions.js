33d5669a2e753bf878f307b36e720bfc0f924685
Andrew <140230860+Andrew-1r@users.noreply.github.com>
2023-08-01 18:02:36 +1000

    adding JS

--- src/js/site_interactions.js
+++ src/js/site_interactions.js
@@ -1,2 +1,11 @@
-// start
-console.log('My JavaScript is alive!');
+// discount code button
+let discount_code = 5000;
+
+const discount_button = document.getElementById('discount-code');
+
+const showDiscountCode = () => {
+    discount_code = discount_code + 1;
+    alert("Your discount code is " + discount_code);
+}
+
+discount_button.addEventListener("click", showDiscountCode);
