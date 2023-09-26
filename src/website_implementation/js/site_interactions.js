7fe71794297d42c74959b84b85f281093f2f767b
Andrew <Andreww.1991@outlook.com>
2023-09-26 16:50:24 +1000

    apply and clear filters javascript added

--- src/website_implementation/js/site_interactions.js
+++ src/website_implementation/js/site_interactions.js
@@ -19,20 +19,53 @@ function search() {
     //future search logic goes here, e.g. combine search terms and filter
 }
 
-//search filter form functionality
+//search filter form open and close
 const filter_window = document.getElementById("search-filter-window");
 const filter_btn = document.getElementById("search-filter-button");
 const close_btn = document.getElementsByClassName("close")[0];
-//open and close
+const apply_filters_btn = document.getElementById("apply-filters-button");
+const clear_filters_btn = document.getElementById("clear-filters-button");
+let current_filters = {};
+
 filter_btn.onclick = function() {
     filter_window.style.display = "block";
 }
 close_btn.onclick = function() {
     filter_window.style.display = "none";
 }
-//close if click off form
+//close if user clicks off form
 window.onclick = function(event) {
   if (event.target == filter_window) {
     filter_window.style.display = "none";
   }
+}
+//apply filters 
+function update_filters() {
+    current_filters ={
+        hatha: document.getElementById('hatha').checked,
+        ashtanga: document.getElementById('ashtanga').checked,  
+        nice_hikes: document.getElementById('nice_hikes').checked,
+        challening_hikes: document.getElementById('challening_hikes').checked,   
+        fiction: document.getElementById('fiction').checked,
+        guitar_books: document.getElementById('guitar_books').checked,
+    };
+}
+  apply_filters_btn.onclick = function() {
+    update_filters()
+    console.log(current_filters);
+}
+//clear filters
+function clear_filters() {
+    current_filters ={
+        hatha: document.getElementById('hatha').checked = false,
+        ashtanga: document.getElementById('ashtanga').checked = false,  
+        nice_hikes: document.getElementById('nice_hikes').checked = false,
+        challening_hikes: document.getElementById('challening_hikes').checked = false,   
+        fiction: document.getElementById('fiction').checked = false,
+        guitar_books: document.getElementById('guitar_books').checked = false,
+    };
+}
+  clear_filters_btn.onclick = function() {
+    clear_filters()
+    console.log(current_filters);
 }
