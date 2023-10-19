bb4ef7943c216887b1d091897c1423cdbc39e9a4
Andrew <Andreww.1991@outlook.com>
2023-10-19 15:15:38 +1000

    JS GET functionality added

--- src/website_implementation/js/results_interactions.js
+++ src/website_implementation/js/results_interactions.js
@@ -1,7 +1,7 @@
 const my_website_code = 'Andrew1991'
 
 const baseURLEvents = "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/community_events/";
-
+const eventsContainer = document.getElementById('events-container');
 
 //const functions
 const triggerFileInput = () => {
@@ -12,6 +12,13 @@ const handleFileChange = () => {
 
 const handleFormSubmit = event => {
 };
+
+//add to handleFormSubmit function pg 39 APIs_2
+// .then(data => {
+//     getCommunityEvents();
+// })
+
+
 
 //fetch events from API
 const getCommunityEvents = () => {
@@ -34,7 +41,28 @@ const getCommunityEvents = () => {
         }
         return response.json()
     })
-    .then(events => console.log(events))
+    .then(events => {
+        console.log(events);
+        while (eventsContainer.firstChild) {
+            eventsContainer.removeChild(eventsContainer.firstChild);
+        }
+        events.forEach(event => {
+            const eventTemplate = `
+            <article class="col-12 col-md-12 col-lg-6">
+                <div class="card" role="group" aria-labelledby="card${event.id}-title" aria-describedby="card${event.id}-desc">
+                    <h2 class="card-header p-2" id="card${event.id}-title">${event.name}</h2>
+                    <img class="card-banner-image" src="${event.photo}" alt="${event.name}">
+                    <p class="card-body-text" p-2">${event.description}</p>
+                    <p class="card-body-text" px-2"><strong>Location:</strong> ${event.location}</p>
+                    <p class="card-body-text" px-2"><strong>Organiser:</strong> ${event.organiser}</p>
+                    <p class="card-body-text" px-2"><strong>Event Type:</strong> ${event.event_type}</p>
+                    <p class="card-body-text" px-2"><strong>Date and Time:</strong> ${new Date(event.date_time).toLocaleString()}</p>
+                </div>
+            </article>
+            `;
+            eventsContainer.innerHTML += eventTemplate;
+        })
+    })
     .catch(error => {
         console.error("Error processing events:", error.message);
         alert("There was a problem loading events. \
@@ -42,3 +70,10 @@ const getCommunityEvents = () => {
     });
 };
 
+//event listeners
+photoFileInputLabel.addEventListener('click', triggerFileInput);
+photoFileInput.addEventListener('change', handleFileChange);
+eventForm.addEventListener('submit', handleFormSubmit);
+
+//setup page on first load
+getCommunityEvents();
