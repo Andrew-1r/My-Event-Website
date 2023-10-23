5bee1442e3cd0838ecbcf09453a588f6fc57bd5e
Andrew <Andreww.1991@outlook.com>
2023-10-24 09:46:59 +1000

    combined get and search functions, improved filter

--- src/website_implementation/js/results_interactions.js
+++ src/website_implementation/js/results_interactions.js
@@ -6,7 +6,7 @@ const eventsContainer = document.getElementById('events-container');
 
 //fetch events from API
 const getCommunityEvents = () => {
-    //do i need this?
+
     const queryParams = {
         website_code: my_website_code,
     }
@@ -54,5 +54,68 @@ const getCommunityEvents = () => {
     });
 };
 
-//setup page on first load
-getCommunityEvents();
+//search filter form open and close
+const filter_window = document.getElementById("search-filter-window");
+const filter_btn = document.getElementById("search-filter-button");
+const close_btn = document.getElementsByClassName("close")[0];
+const apply_filters_btn = document.getElementById("apply-filters-button");
+const clear_filters_btn = document.getElementById("clear-filters-button");
+let current_filters = {};
+
+filter_btn.onclick = function() {
+    filter_window.style.display = "block";
+    current_filters ={
+        workshops: document.getElementById('workshops').checked = false,
+        webinars: document.getElementById('webinars').checked = false,  
+        seminars: document.getElementById('seminars').checked = false,
+        group_activities: document.getElementById('group-activities').checked = false,   
+    };
+}
+close_btn.onclick = function() {
+    filter_window.style.display = "none";
+}
+//close if user clicks off form
+window.onclick = function(event) {
+  if (event.target == filter_window) {
+    filter_window.style.display = "none";
+  }
+}
+//apply filters 
+function update_filters() {
+    current_filters ={
+        workshops: document.getElementById('workshops').checked,
+        webinars: document.getElementById('webinars').checked,  
+        seminars: document.getElementById('seminars').checked,
+        group_activities: document.getElementById('group-activities').checked,  
+    };
+}
+  apply_filters_btn.onclick = function() {
+    update_filters()  
+    filter_window.style.display = "none";
+    search()
+}
+    
+//clear filters
+function clear_filters() {
+    filter_window.style.display = "none";
+    current_filters ={
+        workshops: document.getElementById('workshops').checked = true,
+        webinars: document.getElementById('webinars').checked = true,  
+        seminars: document.getElementById('seminars').checked = true,
+        group_activities: document.getElementById('group-activities').checked = true,   
+    };
+}
+  clear_filters_btn.onclick = function() {
+    clear_filters()
+}
+
+//javascript search button no search logic / functionality yet 
+// - need to update to hold output from filter form
+function search() {
+    const search_value = document.getElementById("search-box").value;
+
+    //future search logic goes here, e.g. combine search terms and filter  
+    console.log(current_filters);
+    console.log(search_value);
+    getCommunityEvents();
+}
